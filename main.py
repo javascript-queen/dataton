6f6fd0917b72e22da26be6af6a557935b733bccb
Anna Traylor <90614620+javascript-queen@users.noreply.github.com>
2023-12-18 18:17:32 +0300

    feat: upload project

---
+++ main.py
@@ -0,0 +1,44 @@
+# python3 -m venv venv
+# . venv/bin/activate
+# pip install streamlit
+# pip install torch torchvision
+# streamlit run main.py
+# python3 download_saved_models.py
+import time
+import streamlit as st
+from PIL import Image
+import style
+
+st.title('Group 10 Style Transfer App')
+
+img = st.sidebar.selectbox(
+    'Выберите Фото',
+    ('selfie.jpg', 'selfietwo.jpg')
+)
+
+style_name = st.sidebar.selectbox(
+    'Выберите Стиль',
+    ('Pop art', 'Art Nouveau', 'Cubism', 'Abstract')
+)
+
+
+model = "saved_models/" + style_name + ".pth"
+input_image = "images/content-images/" + img
+output_image = "images/output-images/" + style_name + "-" + img
+
+st.write('### Source image:')
+image = Image.open(input_image)
+st.image(image, width=400)
+
+clicked = st.button('Stylize')
+        
+if clicked:
+    st.snow()
+    model = style.load_model(model)
+    with st.spinner('Wait for it...'):
+        style.stylize(model, input_image, output_image)
+      
+        st.write('### Output image:')
+        image = Image.open(output_image)
+        st.image(image, width=400)
+    st.success('Done!')
