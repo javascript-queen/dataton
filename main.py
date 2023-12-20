8f0e74b0b153cd6e000abc21b2fcb7880f7f6de3
Anna Traylor <90614620+javascript-queen@users.noreply.github.com>
2023-12-20 08:25:43 +0400

    feat: update main.py

--- main.py
+++ main.py
@@ -3,42 +3,65 @@
 # pip install streamlit
 # pip install torch torchvision
 # streamlit run main.py
-
+# python3 download_saved_models.py
 import time
 import streamlit as st
 from PIL import Image
 import style
+import os
 
 st.title('Group 10 Style Transfer App')
 
-img = st.sidebar.selectbox(
-    'Выберите Фото',
-    ('selfie.jpg', 'selfietwo.jpg')
-)
+# style image paths:
+root_style = "./images/style-images"
 
+# creating a side bar for picking the style of image
 style_name = st.sidebar.selectbox(
-    'Выберите Стиль',
-    ('Pop art', 'Art Nouveau')
+    'Select Style',
+    ('Pop Art', 'Art Nouveau', 'Cubism', 'Abstract')
 )
+path_style = os.path.join(root_style, style_name+".jpg")
 
+# Upload image functionality
+img = None
+uploaded_file = st.file_uploader(
+    "Choose an image...", type=["jpg", "jpeg", "png"])
 
-model = "models/" + style_name + ".pth"
-input_image = "images/content-images/" + img
-output_image = "images/output-images/" + style_name + "-" + img
+show_file = st.empty()
 
-st.write('### Source image:')
-image = Image.open(input_image)
-st.image(image, width=400)
+# checking if user has uploaded any file
+if not uploaded_file:
+    show_file.info("Please Upload an Image")
+else:
+    img = Image.open(uploaded_file)
+    # check required here if file is an image file
+    st.image(img, caption='Uploaded Image.', use_column_width=True)
+    st.image(path_style, caption='Style Image', use_column_width=True)
 
-clicked = st.button('Stylize')
+extensions = [".png", ".jpeg", ".jpg"]
+
+if uploaded_file is not None and any(extension in uploaded_file.name for extension in extensions):
+
+    name_file = uploaded_file.name.split(".")
+    root_model = "./saved_models"
+    model_path = os.path.join(root_model, style_name+".pth")
+
+    img = img.convert('RGB')
+    input_image = img
+
+    root_output = "./images/output-images"
+    output_image = os.path.join(
+        root_output, style_name+"-"+name_file[0]+".jpg")
+
+    clicked = st.button("Stylize")
         
-if clicked:
-    st.snow()
-    model = style.load_model(model)
-    with st.spinner('Wait for it...'):
-        style.stylize(model, input_image, output_image)
-      
-        st.write('### Output image:')
-        image = Image.open(output_image)
-        st.image(image, width=400)
-    st.success('Done!')
+    if clicked:
+        st.snow()
+        model = style.load_model(model_path)
+        with st.spinner('Wait for it...'):
+            style.stylize(model, input_image, output_image)
+          
+            st.write('### Output image:')
+            image = Image.open(output_image)
+            st.image(image, width=400)
+        st.success('Done!')
