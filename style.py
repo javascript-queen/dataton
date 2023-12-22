e3cfd302e64b53a43520d0382b2cd4f00efd7bf6
Anna Traylor <90614620+javascript-queen@users.noreply.github.com>
2023-12-22 09:20:58 +0400

    feat: update style.py

--- style.py
+++ style.py
@@ -1,10 +1,16 @@
+# Импорт модуля для обработки аргументов командной строки.
 import argparse
+# Импорт модуля для взаимодействия с операционной системой.
 import os
+# Импорт системного модуля для доступа к различным переменным и функциям Python.
 import sys
+# Импорт модуля для работы со временем.
 import time
+# Импорт модуля для работы с регулярными выражениями.
 import re
 
 import numpy as np
+# Импорт пакетов Torch
 import torch
 from torch.optim import Adam
 from torch.utils.data import DataLoader
@@ -12,58 +18,64 @@ from torchvision import datasets
 from torchvision import transforms
 import torch.onnx
 
+# Импорт пользовательских утилит
 import utils
+#  Импорт класса TransformerNet из файла transformer_net.py
 from transformer_net import TransformerNet
+# Импорт класса Vgg16 из файла vgg.py
 from vgg import Vgg16
 import streamlit as st
 
-# we will use the conecpt of caching here that is once a user has used a particular model instead of loading
-# it again and again everytime they use it we will cache the model.
+# Здесь мы будем использовать концепцию кэширования, чтобы после того, как пользователь
+# использовал определенную модель, вместо загрузки ее снова и снова при каждом использовании,
+# мы будем кэшировать модель.
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# loading a model
-
+# Загрузка модели
 
 @st.cache_data
 def load_model(model_path):
-
+    # Создание экземпляра модели стиля
     with torch.no_grad():
-        style_model = TransformerNet()  # transformer_net.py contain the style model
+        # Файл transformer_net.py содержит модель стиля
+        style_model = TransformerNet()  
         state_dict = torch.load(model_path)
-        # remove saved deprecated running_* keys in InstanceNorm from the checkpoint
+        # Удаление сохраненных устаревших ключей running_* в InstanceNorm из контрольной точки
         for k in list(state_dict.keys()):
             if re.search(r'in\d+\.running_(mean|var)$', k):
                 del state_dict[k]
+        # Загрузка весов модели стиля
         style_model.load_state_dict(state_dict)
         style_model.to(device)
         style_model.eval()
         return style_model
 
-# we need the content image and the the style model that we have loaded
-# with load_model function
-
+# Нам нужно изображение контента и загруженная модель стиля,
+# загруженная с помощью функции load_model
 
 @st.cache_data
 def stylize(_style_model, content_image, output_image):
 
-    # if the content image is a path then
+    # Eсли изображение контента представлено в виде пути к файлу
     if type(content_image) == "str":
         content_image = utils.load_image(
             content_image)
+    # Преобразование изображения контента
     content_transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Lambda(lambda x: x.mul(255))
     ])
     content_image = content_transform(content_image)
 
-    # to treat a single image like a batch
+    # Преобразование одного изображения как пакета
     content_image = content_image.unsqueeze(0).to(device)
 
     with torch.no_grad():
+        # Получение стилизованного изображения
         output = _style_model(content_image).cpu()
 
-    # output image here is the path to the output image
+    # Сохранение стилизованного изображения по указанному пути
     img = utils.save_image(output_image, output[0])
     return img
 
